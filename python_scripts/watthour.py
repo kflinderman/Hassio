9eb5a2cf301513f1189ae204498b0dfcc05cdbbc
kflinderman <kenny.linderman@gmail.com>
2018-10-10 20:46:41 -0400

    Updated to 0.79.3

--- python_scripts/watthour.py
+++ python_scripts/watthour.py
@@ -2,9 +2,10 @@ wattHour = 0
 cpuWhr = 0
 totalWhr = 0
 hueEntities = ['sensor.rock_lamp_light_hours', 'sensor.floor_lamp_light_hours', 'sensor.chair_lamp_light_hours', 'sensor.swirl_lamp_light_hours', 'sensor.oven_light_hours', 'sensor.front_door_light_hours', 'sensor.basement_1_light_hours', 'sensor.basement_2_light_hours', 'sensor.basement_3_light_hours', 'sensor.basement_4_light_hours', 'sensor.dining_room_light_hours', 'sensor.dining_room_light_hours', 'sensor.dining_room_light_hours', 'sensor.dining_room_light_hours', 'sensor.dining_room_light_hours', 'sensor.deck_light_hours', 'sensor.sewing_light_hours']
-lightbulbEntities = ['sensor.basement_light_hours', 'sensor.bedroom_fan_hours', 'sensor.bedroom_fan_hours', 'sensor.office_fan_hours', 'sensor.office_fan_hours', 'sensor.office_fan_hours', 'sensor.craft_room_fan_hours', 'sensor.craft_room_fan_hours', 'sensor.craft_room_fan_hours', 'sensor.master_bathroom_hours', 'sensor.master_bathroom_hours', 'sensor.master_bathroom_hours']
+lightbulbEntities = ['sensor.washroom_light_hours', 'sensor.basement_light_hours', 'sensor.bedroom_fan_hours', 'sensor.bedroom_fan_hours', 'sensor.office_fan_hours', 'sensor.office_fan_hours', 'sensor.office_fan_hours', 'sensor.craft_room_fan_hours', 'sensor.craft_room_fan_hours', 'sensor.craft_room_fan_hours', 'sensor.master_bathroom_hours', 'sensor.master_bathroom_hours', 'sensor.master_bathroom_hours']
 compEntities = ['sensor.computer_hours']
 ledEntities = ['sensor.prep_area_hours', 'sensor.guest_bedroom_fan_hours', 'sensor.guest_bedroom_fan_hours', 'sensor.guest_bedroom_fan_hours', 'sensor.guest_bedroom_fan_hours', 'sensor.guest_bedroom_fan_hours', 'sensor.guest_bedroom_fan_hours', 'sensor.guest_bedroom_fan_hours', 'sensor.guest_bedroom_fan_hours']
+marketEntities = ['sensor.market_light_hours']
 
 for entity_id in hueEntities:
   # copy it's state
@@ -26,6 +27,13 @@ for entity_id in ledEntities:
   
   if state.state is not 'unknown':
      wattHour = wattHour + (int(float(state.state)) * 20)
+	 
+for entity_id in marketEntities:
+  # copy it's state
+  state = hass.states.get(entity_id)
+  
+  if state.state is not 'unknown':
+     wattHour = wattHour + (int(float(state.state)) * 110)
 	 
 for entity_id in compEntities:
   # copy it's state
