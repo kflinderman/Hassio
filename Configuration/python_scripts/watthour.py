3273779ab91af5e3eee4e50e9dcd455e8c8e47c5
kflinderman <kenny.linderman@gmail.com>
2020-01-14 22:43:58 -0500

    Moving Rooms Around
    Getting started by moving most of the office down to the basement. Office will now be the nursery. Most of the stuff has been trasnported over, the leftover stuff I don't think will matter much.

--- Configuration/python_scripts/watthour.py
+++ Configuration/python_scripts/watthour.py
@@ -2,10 +2,11 @@ wattHour = 0
 cpuWhr = 0
 totalWhr = 0
 hueEntities = ['sensor.tripod_light_hours', 'sensor.rock_lamp_light_hours', 'sensor.floor_lamp_light_hours', 'sensor.chair_lamp_light_hours', 'sensor.swirl_lamp_light_hours', 'sensor.oven_light_hours', 'sensor.front_door_light_hours', 'sensor.basement_1_light_hours', 'sensor.basement_2_light_hours', 'sensor.basement_3_light_hours', 'sensor.basement_4_light_hours', 'sensor.dining_room_light_hours', 'sensor.dining_room_light_hours', 'sensor.dining_room_light_hours', 'sensor.dining_room_light_hours', 'sensor.dining_room_light_hours', 'sensor.deck_light_hours', 'sensor.sewing_light_hours']
-lightbulbEntities = ['sensor.washroom_light_hours', 'sensor.basement_light_hours', 'sensor.bedroom_fan_hours', 'sensor.bedroom_fan_hours', 'sensor.office_fan_hours', 'sensor.office_fan_hours', 'sensor.office_fan_hours', 'sensor.craft_room_fan_hours', 'sensor.craft_room_fan_hours', 'sensor.craft_room_fan_hours', 'sensor.master_bathroom_hours', 'sensor.master_bathroom_hours', 'sensor.master_bathroom_hours']
+lightbulbEntities = ['sensor.basement_light_hours', 'sensor.washroom_light_hours', 'sensor.basement_light_hours', 'sensor.bedroom_fan_hours', 'sensor.bedroom_fan_hours', 'sensor.nursery_fan_hours', 'sensor.nursery_fan_hours', 'sensor.nursery_fan_hours', 'sensor.craft_room_fan_hours', 'sensor.craft_room_fan_hours', 'sensor.craft_room_fan_hours', 'sensor.master_bathroom_hours', 'sensor.master_bathroom_hours', 'sensor.master_bathroom_hours']
 compEntities = ['sensor.computer_hours']
 ledEntities = ['sensor.prep_area_hours', 'sensor.guest_bedroom_fan_hours', 'sensor.guest_bedroom_fan_hours', 'sensor.guest_bedroom_fan_hours', 'sensor.guest_bedroom_fan_hours', 'sensor.guest_bedroom_fan_hours', 'sensor.guest_bedroom_fan_hours', 'sensor.guest_bedroom_fan_hours', 'sensor.guest_bedroom_fan_hours']
 marketEntities = ['sensor.market_light_hours', 'sensor.sun_room_market_hours']
+halogenEntities = ['sensor.entrance_light_hours', 'sensor.entrance_light_hours']
 
 for entity_id in hueEntities:
   # copy it's state
@@ -35,6 +36,13 @@ for entity_id in marketEntities:
   if state.state is not 'unknown':
      wattHour = wattHour + (int(float(state.state)) * 110)
 	 
+for entity_id in halogenEntities:
+  # copy it's state
+  state = hass.states.get(entity_id)
+  
+  if state.state is not 'unknown':
+     wattHour = wattHour + (int(float(state.state)) * 26)
+     
 for entity_id in compEntities:
   # copy it's state
   state = hass.states.get(entity_id)
